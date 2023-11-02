25a6be2eec25ab4aa3b78ef0f019812051f791b6
Zhang Maiyun <me@maiyun.me>
2023-11-02 11:16:43 -0700

    Further overengineer and cast the Rust way

--- dotest.c
+++ dotest.c
@@ -14,9 +14,9 @@
 #include "questions.h"
 
 static const struct data_index {
-    struct question_t *pool;
-    size_t pool_size;
-    char *human_readable;
+    const struct question_t *pool;
+    const size_t pool_size;
+    const char *human_readable;
 } QUESTIONPOOLS[] = {
     {ised_basic_en, ised_basic_en_count, "ISED Canada Basic in English"},
     {ised_basic_fr, ised_basic_fr_count, "ISED Canada Basic in French"},
@@ -33,24 +33,38 @@ static const struct data_index {
 static const size_t QUESTIONPOOLS_SIZE = 10;
 
 #if (ENABLE_GTK + 0) == 1
-void display_image(const void *image_data, const size_t length, const char *title) {
+/// Display an image with the GTK backend
+/// @param filename The filename of the image to display
+void display_image(const char *filename) {
+    const struct image_info_t *image = in_word_set(filename, strlen(filename));
+    if (image == NULL) {
+        fprintf(stderr, "Image %s not found\n", filename);
+        return;
+    }
+    const void *image_data = image->content;
+    const size_t length = image->length;
+    printf("Displaying image %s length %zu\n", filename, image->length);
+
     GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     GdkPixbufLoader *loader = gdk_pixbuf_loader_new();
-    GdkPixbuf *pixbuf;
-    GtkWidget* image;
-
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_window_set_default_size(GTK_WINDOW(window), 50, 30);
-    gtk_window_set_title(GTK_WINDOW(window), title);
+    gtk_window_set_title(GTK_WINDOW(window), filename);
     gtk_window_set_destroy_with_parent(GTK_WINDOW(window), TRUE);
 
     gdk_pixbuf_loader_write(loader, image_data, length, NULL);
     gdk_pixbuf_loader_close(loader, NULL);
-    pixbuf = gdk_pixbuf_loader_get_pixbuf(loader);
-    image = gtk_image_new_from_pixbuf(pixbuf);
-    gtk_container_add(GTK_CONTAINER(window), image);
+    GdkPixbuf *pixbuf = gdk_pixbuf_loader_get_pixbuf(loader);
+    GtkWidget *image_widget = gtk_image_new_from_pixbuf(pixbuf);
+    gtk_container_add(GTK_CONTAINER(window), image_widget);
     gtk_widget_show_all(window);
     gtk_main();
+}
+#else
+/// Dummy function for non-GTK builds
+/// @param filename The filename of the image to display
+void display_image(const char *filename) {
+    printf("Please open %s in your favourite image viewer.\n", filename);
 }
 #endif
 
@@ -61,12 +75,11 @@ void display_image(const void *image_data, const size_t length, const char *titl
 /// @return The integer.
 uint64_t input_u64(const char *prompt, uint64_t max) {
     while (1) {
-        char buf[10];
-        uint64_t result;
         printf("%s", prompt);
         fflush(stdout);
+        char buf[10] = {0};
         fgets(buf, 10, stdin);
-        result = strtoul(buf, NULL, 10);
+        uint64_t result = strtoul(buf, NULL, 10);
         if (result < max)
             return result;
         puts("Invalid choice");
@@ -87,14 +100,14 @@ size_t input_qb(void) {
 /// @param size The size of the array.
 /// @return The array. Needs to be `free`d.
 size_t *random_indices(size_t size, pcg32_random_t *rng) {
-    size_t *indices = malloc(size * sizeof(size_t));
+    size_t *indices = (size_t *) malloc(size * sizeof(size_t));
     for (size_t i = 0; i < size; ++i)
         indices[i] = i;
 
     // Fisher-Yates shuffle
     for (size_t i = size - 1; i > 0; --i) {
-        uint32_t j = pcg32_boundedrand_r(rng, i + 1);
-        size_t tmp = indices[i];
+        const uint32_t j = pcg32_boundedrand_r(rng, i + 1);
+        const size_t tmp = indices[i];
         indices[i] = indices[j];
         indices[j] = tmp;
     }
@@ -105,11 +118,12 @@ size_t *random_indices(size_t size, pcg32_random_t *rng) {
 /// Test a single question.
 /// @param pool the question pool.
 /// @param index the index of the question in the pool. It must be valid.
-/// @return 1 if the user answered correctly, 0 otherwise.
-uint32_t test_single(struct question_t *pool, size_t index, pcg32_random_t *rng) {
-    size_t correct_pos = pcg32_boundedrand_r(rng, 4), incorrect_pos_now;
-    uint16_t choice;
-    struct question_t *question = &pool[index];
+/// @return 1 if the user answered correctly, 0 otherwise. 2 if the user wants
+/// to quit.
+uint16_t test_single(const struct question_t *pool, const size_t index, pcg32_random_t *rng) {
+    const struct question_t *question = pool + index;
+    size_t incorrect_pos_now;
+    const size_t correct_pos = (size_t) pcg32_boundedrand_r(rng, 4);
     printf("%s %s\n", question->number, question->question);
     putchar('\n');
     for (incorrect_pos_now = 0; incorrect_pos_now < correct_pos; ++incorrect_pos_now)
@@ -120,19 +134,12 @@ uint32_t test_single(struct question_t *pool, size_t index, pcg32_random_t *rng)
     putchar('\n');
     for (size_t i = 0; i < question->image_count; ++i) {
         const char *filename = question->images[i];
-        const struct image_info_t *image = in_word_set(filename, strlen(filename));
-        if (image == NULL) {
-            fprintf(stderr, "Image %s not found\n", filename);
-            continue;
-        }
-        printf("Displaying image %zu length %zu\n", i + 1, image->length);
-#if (ENABLE_GTK + 0) == 1
-        display_image(image->content, image->length, filename);
-#endif
+        display_image(filename);
     }
-    choice = (uint16_t) input_u64("Choice: ", 5);
+    uint16_t choice = (uint16_t) input_u64("Choice: ", 5);
     if (choice == 0)
-        choice = 1;
+        // EOF or some non-numeric input
+        return 2;
     putchar('\n');
     if (choice == correct_pos + 1)
         puts("Correct!");
@@ -144,17 +151,21 @@ uint32_t test_single(struct question_t *pool, size_t index, pcg32_random_t *rng)
 /// Test the user on a question pool.
 /// @param pool The question pool.
 /// @param pool_size The size of the question pool.
-void offer_test(struct question_t *pool, size_t pool_size, pcg32_random_t *rng) {
+void offer_test(const struct question_t *pool, const size_t pool_size, pcg32_random_t *rng) {
     // Do I care about performance? Not really. Just work with cache misses.
     // Create a shuffled array of indices
-    size_t *indices = random_indices(pool_size, rng);
-    uint32_t correct_count = 0, total = 0;
+    const size_t *indices = random_indices(pool_size, rng);
+    uint16_t correct_count = 0, total = 0;
     for (; total < pool_size; ++total) {
-        correct_count += test_single(pool, indices[total], rng);
+        const uint16_t result = test_single(pool, indices[total], rng);
+        if (result == 2)
+            break;
+        correct_count += result;
         printf("Score: %d/%d = %g%%\n", correct_count, total + 1, 100.0 * correct_count / (total + 1));
         putchar('\n');
     }
-    free(indices);
+    free((void *) indices);
+    printf("Final score: %d/%d = %g%%\n", correct_count, total, 100.0 * correct_count / total);
 }
 
 int main(int argc, char **argv) {
